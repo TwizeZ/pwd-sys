20c50b120d73ca6d465bf183624184d867f6c7dd
Felix <felix.famlarsson@gmail.com>
2022-04-11 22:20:15 +0200

    Added menu and database structure

--- pwd.py
+++ pwd.py
@@ -7,6 +7,36 @@ import random
 # from time import sleep
    
 
+class Passwords:
+    def __init__(self, name : str, username : str, email : str, password : str, notes : str):
+        # def for format in passwords.txt
+        self.name = name
+        self.username = username
+        self.email = email
+        self.password = password
+        self.notes = notes
+        # self.created = created
+
+
+
+def load_question():
+    questions = []
+    with open("passwords.txt", "r", encoding="utf8") as f:
+        for line in f.readlines():
+            selection = line.split("/")
+            pwd = Passwords(selection[0],
+                            selection[1],
+                            selection[2].split(","))
+            
+            questions.append(pwd)
+    return questions
+
+def question_quantity():
+    with open("passwords.txt", "r", encoding="utf8") as fp:
+        qtot = len(fp.readlines())
+    return qtot    
+
+
 
 def main():
     pass
